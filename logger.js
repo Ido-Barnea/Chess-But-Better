e022ea515fc712ccd1bd03ee799c66df1cb1798a
Omoor <omergolan38@gmail.com>
2023-10-10 19:31:20 +0300

    [#7] Updated Logs UI

--- logger.js
+++ logger.js
@@ -1,8 +1,11 @@
 class Logger {
     static log(message) 
     {
+        const logbox = document.getElementById("logbox");
         // Log the message to the console
         console.log(message);
         document.getElementById("logs").innerHTML += message + "<br />";
+        //scroll the logbox to the last log;
+        logbox.scrollTop = logbox.scrollHeight;
     }
 }
