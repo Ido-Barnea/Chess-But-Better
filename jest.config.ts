f5b1b08cb113e511500d72b70c5cb5087c78ca5e
Ido-Barnea <dodo.barnea@gmail.com>
2023-10-19 20:19:15 +0300

    [trivial] Configured npm, jest, and others

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default {
+  displayName: 'chess-but-better',
+  coverageDirectory: './coverage/chess-but-better',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
