4d330b7cd95d81073a0dfeec1900cf58ad9ba5d7
Omoor <omergolan38@gmail.com>
2023-10-31 19:27:56 +0200

    [#65] Reset config and index.html

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: "chess-but-better",
-  coverageDirectory: "./coverage/chess-but-better",
+  displayName: 'chess-but-better',
+  coverageDirectory: './coverage/chess-but-better',
   testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
-    "<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)",
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
   ],
 };
