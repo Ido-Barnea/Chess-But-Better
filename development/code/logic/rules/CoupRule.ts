8f0f99575181b7fd2ff72cc9a7d52892346d52fd
Ido-Barnea <dodo.barnea@gmail.com>
2024-01-26 19:48:37 +0200

    [#159] Picking random pieces to desert

--- development/code/logic/rules/CoupRule.ts
+++ development/code/logic/rules/CoupRule.ts
@@ -16,21 +16,25 @@ export class CoupRule extends BaseRule {
       });
       return result;
     };
+
     const onTrigger = () => {
       game.getPlayers().forEach((player) => {
         if (player.inDebtForTurns === 2 && player === game.getCurrentPlayer()) {
           player.inDebtForTurns = -1;
           const playerPieces = game.getPieces().filter(piece => piece.player === player);
           const randomAmountOfPieces = Math.floor(Math.random() * (playerPieces.length - 1) / 2) + 1;
+          
           Logger.logRule(`${player.color} is deep in debt. ${randomAmountOfPieces} of their pieces desert.`);
 
           let desertedPiecesCounter = 0;
-          playerPieces.forEach(piece => {
-            if (desertedPiecesCounter < randomAmountOfPieces && !(piece instanceof King)) {
-              changePieceToAnotherPlayer(piece);
-              desertedPiecesCounter++;
-            }
-          });
+          while (desertedPiecesCounter < randomAmountOfPieces) {
+            const randomPieceIndex = Math.floor(Math.random() * (playerPieces.length - 1)) + 1;
+            const piece = playerPieces[randomPieceIndex];
+            if (piece instanceof King) continue;
+
+            changePieceToAnotherPlayer(piece);
+            desertedPiecesCounter++;
+          }
         }
       });
     };
