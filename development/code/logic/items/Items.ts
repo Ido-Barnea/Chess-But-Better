35db56ca6ad4f6cc5d3f80e56d6627cac8adfb9a
Ido-Barnea <dodo.barnea@gmail.com>
2024-02-02 14:04:34 +0200

    [trivial] Separated Inventory class to different file

--- development/code/logic/items/Items.ts
+++ development/code/logic/items/Items.ts
@@ -1,36 +1,7 @@
 import { Logger } from '../../ui/Logger';
 import { Piece } from '../pieces/Pieces';
 import { Position } from '../pieces/PiecesUtilities';
-import { Player, PlayerColors } from '../Players';
-
-export class Inventory {
-  items: Array<Item> = [];
-
-  addItem(playerColor: PlayerColors, item: Item) {
-    this.items.push(item);
-    Logger.logGeneral(`${playerColor} received a ${item.name}.`);
-  }
-
-  removeItem(item: Item) {
-    const index = this.items.indexOf(item);
-    if (index !== -1) {
-      this.items.splice(index, 1);
-      Logger.logGeneral(`${item.name} was destroyed.`);
-    }
-  }
-
-  toHTMLElement(): HTMLElement {
-    const inventoryElement = document.createElement('ul');
-    this.items.forEach((item) => {
-      const inventoryItemElement = document.createElement('li');
-      inventoryItemElement.innerHTML = item.name;
-
-      inventoryElement.appendChild(inventoryItemElement);
-    });
-
-    return inventoryElement;
-  }
-}
+import { Player } from '../Players';
 
 interface ItemType {
     name: string,
