c316ee27aa806bcacd807bda78ad8d7be7ad9105
Ido-Barnea <dodo.barnea@gmail.com>
2024-02-14 19:24:58 +0200

    [trivial] Fixed funny bug

--- development/code/logic/rules/PiecesCanFallOffTheBoardRule.ts
+++ development/code/logic/rules/PiecesCanFallOffTheBoardRule.ts
@@ -1,17 +1,11 @@
 import { game } from '../../Game';
-import { KillLog } from '../../ui/logs/Log';
 import { BaseRule } from './BaseRule';
 
 export class PiecesCanFallOffTheBoardRule extends BaseRule {
   constructor(isRevealed = false) {
     const description = 'Pieces can fall off the board.';
     const condition = () => !!game.getFellOffTheBoardPiece();
-    const onTrigger = () => {
-      const fellOffTheBoard = game.getFellOffTheBoardPiece();
-      if (!fellOffTheBoard) return;
-
-      new KillLog(fellOffTheBoard, 'gravity').addToQueue();
-    };
+    const onTrigger = () => { return; };
 
     super(description, isRevealed, condition, onTrigger);
   }
