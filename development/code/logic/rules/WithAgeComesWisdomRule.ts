1f921769132f8b8ca0aa04b2a93a54e519e220ba
Ido-Barnea <dodo.barnea@gmail.com>
2024-02-21 15:45:57 +0200

    [#208] Increased number of turns for "with age comes wisdom" rule

--- development/code/logic/rules/WithAgeComesWisdomRule.ts
+++ development/code/logic/rules/WithAgeComesWisdomRule.ts
@@ -5,13 +5,13 @@ import { BaseRule } from './BaseRule';
 export class WithAgeComesWisdomRule extends BaseRule {
   constructor(isRevealed = false) {
     const description = 'With age comes wisdom.';
-    const condition = () => game.getRoundCounter() === 20;
+    const condition = () => game.getRoundCounter() === 50;
     const onTrigger = () => {
       new RuleLog(
         'Children of war, you have grown old. Each player gains five XP.',
       ).addToQueue();
       game.getPlayers().forEach((player) => {
-        player.xp += 5;
+        player.xp += 10;
       });
     };
 
