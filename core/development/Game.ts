5d5c02c38ebace7b612ab10a730b2d9be68b25a8
Ido-Barnea <dodo.barnea@gmail.com>
2024-02-29 17:41:27 +0200

    [trivial] Minor fixes

--- core/development/Game.ts
+++ core/development/Game.ts
@@ -106,10 +106,9 @@ function checkForUpgradeablePawns() {
     const piece = pieces[index];
     if (piece instanceof Pawn && piece.position) {
       const whitePawnReachedEndOfBoard =
-        piece.player === whitePlayer && piece.position?.coordinates[1] === 0;
+        piece.player === whitePlayer && piece.position.coordinates[1] === 0;
       const blackPawnReachedEndOfBoard =
-        piece.player === blackPlayer &&
-        piece.position?.coordinates[1] === BOARD_WIDTH - 1;
+        piece.player === blackPlayer && piece.position.coordinates[1] === BOARD_WIDTH - 1;
 
       if (whitePawnReachedEndOfBoard || blackPawnReachedEndOfBoard) {
         piece.position.boardId = VOID_BOARD_ID;
