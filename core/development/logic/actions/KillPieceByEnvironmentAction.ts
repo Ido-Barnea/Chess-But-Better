238312d5a4e9e93d7dded8ee8c1e7bb0effff4c8
Ido-Barnea <dodo.barnea@gmail.com>
2024-04-05 18:17:01 +0300

    Fixed piece being killed by trap logic

--- core/development/logic/actions/KillPieceByEnvironmentAction.ts
+++ core/development/logic/actions/KillPieceByEnvironmentAction.ts
@@ -3,7 +3,6 @@ import { ActionResult } from './types/ActionResult';
 import { KillPieceAction } from './KillPieceAction';
 import { KillLog } from '../../ui/logs/Log';
 import { game } from '../../Game';
-import { VOID_BOARD_ID } from '../../Constants';
 
 export class KillPieceByEnvironment extends KillPieceAction {
   private killingSource: string;
@@ -14,9 +13,6 @@ export class KillPieceByEnvironment extends KillPieceAction {
   }
 
   execute(): ActionResult {
-    if (!this.killedPiece.position) return ActionResult.FAILURE;
-    this.killedPiece.position.boardId = VOID_BOARD_ID;
-    
     super.execute();
 
     new KillLog(this.killedPiece, this.killingSource).addToQueue();
--- core/development/logic/actions/KillPieceByFallingOffTheBoardAction.ts
+++ core/development/logic/actions/KillPieceByFallingOffTheBoardAction.ts
@@ -1,8 +1,17 @@
+import { VOID_BOARD_ID } from '../../Constants';
 import { BasePiece } from '../pieces/abstract/BasePiece';
 import { KillPieceByEnvironment } from './KillPieceByEnvironmentAction';
+import { ActionResult } from './types/ActionResult';
 
 export class KillPieceByFallingOffTheBoardAction extends KillPieceByEnvironment {
   constructor(killedPiece: BasePiece) {
     super(killedPiece, 'the void');
+  }
+
+  execute(): ActionResult {
+    if (!this.killedPiece.position) return ActionResult.FAILURE;
+    this.killedPiece.position.boardId = VOID_BOARD_ID;
+
+    return super.execute();
   }
 }
