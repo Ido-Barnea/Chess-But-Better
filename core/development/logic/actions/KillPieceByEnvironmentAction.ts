ddfe981534582dadfe95941919870b11b575daf0
Ido-Barnea <dodo.barnea@gmail.com>
2024-04-05 18:14:55 +0300

    Fixed pieces falling off the board logic

--- core/development/logic/actions/KillPieceByEnvironmentAction.ts
+++ core/development/logic/actions/KillPieceByEnvironmentAction.ts
@@ -3,6 +3,7 @@ import { ActionResult } from './types/ActionResult';
 import { KillPieceAction } from './KillPieceAction';
 import { KillLog } from '../../ui/logs/Log';
 import { game } from '../../Game';
+import { VOID_BOARD_ID } from '../../Constants';
 
 export class KillPieceByEnvironment extends KillPieceAction {
   private killingSource: string;
@@ -13,6 +14,9 @@ export class KillPieceByEnvironment extends KillPieceAction {
   }
 
   execute(): ActionResult {
+    if (!this.killedPiece.position) return ActionResult.FAILURE;
+    this.killedPiece.position.boardId = VOID_BOARD_ID;
+    
     super.execute();
 
     new KillLog(this.killedPiece, this.killingSource).addToQueue();
