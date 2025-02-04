5059aa6a955e4057af38e0c8a702c358acafa8f6
Ido Barnea <dodo.barnea@gmail.com>
2025-02-04 13:32:39 +0200

    Fixed piece being dragged with background

--- chess-but-better/src/view/game/pieces/piece.tsx
+++ chess-but-better/src/view/game/pieces/piece.tsx
@@ -26,6 +26,7 @@ export const Piece: FC<PieceProps> = ({piece}) => {
                 fill: piece.team.name,
                 opacity: isDragging ? 0.5 : 1,
                 cursor: "grab",
+                transform: 'translate(0, 0)',
             }}
         >
             <Resource width="100%" height="100%" {...(Resource as any)} />
