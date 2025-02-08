315d617fd68540131fef6c8aa1d0b4ce3a7f949e
Ido Barnea <dodo.barnea@gmail.com>
2025-02-08 17:46:37 +0200

    hot reload

--- chess-but-better/vite.config.ts
+++ chess-but-better/vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
+const PORT = 3000;
+
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    port: PORT,
+    open: true,
+    watch: {
+      ignored: []
+    }
+  }
 })
