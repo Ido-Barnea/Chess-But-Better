8007ac8cd2d3decba0ffa95458f42c41b5698ef5
Ido Barnea <dodo.barnea@gmail.com>
2025-02-14 15:28:09 +0200

    Fixed King not spawning in hell

--- chess-but-better/src/controller/pieces/utilities/PieceUtilities.ts
+++ chess-but-better/src/controller/pieces/utilities/PieceUtilities.ts
@@ -1,7 +1,7 @@
 import { PieceResources } from "../../../model/piece/utilities/PieceResources";
 import { ITeam } from "../../../model/player/team/abstract/ITeam";
 import { PieceIcon } from "../types/PieceIcons";
-import { PieceType } from "../types/Pieces";
+import { Pieces, PieceType } from "../types/Pieces";
 import KingResource from '../../../assets/images/pieces/base/KingResource.svg?react';
 import QueenResource from '../../../assets/images/pieces/base/QueenResource.svg?react';
 import RookResource from '../../../assets/images/pieces/base/RookResource.svg?react';
@@ -11,18 +11,18 @@ import PawnResource from '../../../assets/images/pieces/base/PawnResource.svg?re
 
 export class PieceUtilities {
   private static pieceResources: Record<string, PieceResources> = {
-    'pawn-white': new PieceResources('White Pawn', PieceIcon.WHITE_PAWN, PawnResource),
-    'pawn-black': new PieceResources('Black Pawn', PieceIcon.BLACK_PAWN, PawnResource),
-    'knight-white': new PieceResources('White Knight', PieceIcon.WHITE_KNIGHT, KnightResource),
-    'knight-black': new PieceResources('Black Knight', PieceIcon.BLACK_KNIGHT, KnightResource),
-    'bishop-white': new PieceResources('White Bishop', PieceIcon.WHITE_BISHOP, BishopResource),
-    'bishop-black': new PieceResources('Black Bishop', PieceIcon.BLACK_BISHOP, BishopResource),
-    'rook-white': new PieceResources('White Rook', PieceIcon.WHITE_ROOK, RookResource),
-    'rook-black': new PieceResources('Black Rook', PieceIcon.BLACK_ROOK, RookResource),
-    'queen-white': new PieceResources('White Queen', PieceIcon.WHITE_QUEEN, QueenResource),
-    'queen-black': new PieceResources('Black Queen', PieceIcon.BLACK_QUEEN, QueenResource),
-    'king-white': new PieceResources('White King', PieceIcon.WHITE_KING, KingResource),
-    'king-black': new PieceResources('Black King', PieceIcon.BLACK_KING, KingResource),
+    'pawn-white': new PieceResources(Pieces.PAWN, PieceIcon.WHITE_PAWN, PawnResource),
+    'pawn-black': new PieceResources(Pieces.PAWN, PieceIcon.BLACK_PAWN, PawnResource),
+    'knight-white': new PieceResources(Pieces.KNIGHT, PieceIcon.WHITE_KNIGHT, KnightResource),
+    'knight-black': new PieceResources(Pieces.KNIGHT, PieceIcon.BLACK_KNIGHT, KnightResource),
+    'bishop-white': new PieceResources(Pieces.BISHOP, PieceIcon.WHITE_BISHOP, BishopResource),
+    'bishop-black': new PieceResources(Pieces.BISHOP, PieceIcon.BLACK_BISHOP, BishopResource),
+    'rook-white': new PieceResources(Pieces.ROOK, PieceIcon.WHITE_ROOK, RookResource),
+    'rook-black': new PieceResources(Pieces.ROOK, PieceIcon.BLACK_ROOK, RookResource),
+    'queen-white': new PieceResources(Pieces.QUEEN, PieceIcon.WHITE_QUEEN, QueenResource),
+    'queen-black': new PieceResources(Pieces.QUEEN, PieceIcon.BLACK_QUEEN, QueenResource),
+    'king-white': new PieceResources(Pieces.KING, PieceIcon.WHITE_KING, KingResource),
+    'king-black': new PieceResources(Pieces.KING, PieceIcon.BLACK_KING, KingResource),
   };
 
   static getPieceResources(pieceType: PieceType, team: ITeam) {
